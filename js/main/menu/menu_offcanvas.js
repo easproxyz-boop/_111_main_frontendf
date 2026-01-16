fa93888feb3fc75a1cb0718522308ea5711b0582
easproxyz-boop <erwina.aquino@Erwins-MacBook-Pro.local>
2026-01-16 13:14:08 +0800

    first commit

--- js/main/menu/menu_offcanvas.js
+++ js/main/menu/menu_offcanvas.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function () {
 $.ajax({
     url: APP_API_ENDPOINT + '/getdata/useraccount_management_users_offcanvas_menu_uuu8888',
@@ -9,72 +11,94 @@ $.ajax({
     success: function (res) {
 
         const data = res?.data || {};
-        let childPage_ATTENDANCE_SYSTEM = '';
-
-        // =========================
-        // ATTENDANCE SYSTEM ACCESS
-        // =========================
-        if ((data.dt_access_attendance_system || '').toUpperCase() === 'YES') {
-
-            childPage_ATTENDANCE_SYSTEM = `
-                <!-- ATTENDANCE SYSTEM DROPDOWN -->
-                <a 
-                    id="asidebari_at_attendance_system_dropdown"
-                    class="asidebar-link-load-dropdown list-group-item list-group-item-action d-flex align-items-center fw-semibold rounded-3 mb-1 fs-5"
-                    data-bs-toggle="collapse"
-                    href="#collapseAttendanceSystem"
-                    role="button"
-                    aria-expanded="false"
-                    aria-controls="collapseAttendanceSystem">
-                    <i class="ri-government-line me-2"></i>
-                    Human Resources
-                    <i class="ri-arrow-down-s-line ms-auto"></i>
-                </a>
-
-                <!-- ATTENDANCE SUB MENU -->
-                <div class="collapse ps-3" id="collapseAttendanceSystem">
-
-                    <a href="/pages/maingeneral/menu/human_resources/employee_profile_info.html"
-                       id="asidebari_at_employee_profile"
-                       class="asidebar-link-load-000 list-group-item list-group-item-action fw-semibold border-0 rounded-3 mb-1 fs-5">
-                        <i class="ri-user-line me-2"></i> Employee Profile
-                    </a>
-
-                    <a href="/pages/attendance_system/at_logs.html"
-                       class="asidebar-link-load-000 list-group-item list-group-item-action fw-semibold border-0 rounded-3 mb-1 fs-5">
-                        <i class="ri-time-line me-2"></i> Attendance Tracking
-                    </a>
-
-                    <a href="/pages/maingeneral/menu/human_resources/leave_and_requests.html"
-                     id="asidebari_at_employee_leave_and_requests"
-                       class="asidebar-link-load-000 list-group-item list-group-item-action fw-semibold border-0 rounded-3 mb-1 fs-5">
-                        <i class="ri-time-line me-2"></i> Leave & Requests
-                    </a>
 
 
 
-                    <a href="/pages/maingeneral/menu/human_resources/view_attendance_and_dtr.html"
-                     id="asidebari_at_employee_view_attendance_and_dtr"
-                       class="asidebar-link-load-000 list-group-item list-group-item-action fw-semibold border-0 rounded-3 mb-1 fs-5">
-                        <i class="ri-time-line me-2"></i> View attendance & DTR
-                    </a>
+  let childPage_HUMAN_RESOURCES_AND_MANAGEMENT_OFFICE = '';
 
 
-                    <a href="/pages/maingeneral/menu/human_resources/payroll_integration.html"
-                     id="asidebari_at_employee_payroll_integration"
-                       class="asidebar-link-load-000 list-group-item list-group-item-action fw-semibold border-0 rounded-3 mb-1 fs-5">
-                        <i class="ri-time-line me-2"></i> Payroll Integration
-                    </a>
 
 
-                    <a href="/pages/attendance_system/at_reports.html"
-                       class="asidebar-link-load-000 list-group-item list-group-item-action fw-semibold border-0 rounded-3 mb-1 fs-5">
-                        <i class="ri-file-chart-line me-2"></i> Reports
-                    </a>
 
-                </div>
-            `;
-        }
+
+
+
+  if ((data.dt_access_attendance_system || '').toUpperCase() === 'YES') {
+    childPage_HUMAN_RESOURCES_AND_MANAGEMENT_OFFICE = `
+      <!-- HUMAN RESOURCE AND MANAGEMENT OFFICE DROPDOWN -->
+      <a 
+        id="asidebari_at_human_resources_and_management_office_dropdown"
+        class="asidebar-link-load-dropdown list-group-item list-group-item-action d-flex align-items-center fw-semibold rounded-3 mb-1 fs-5"
+        data-bs-toggle="collapse"
+        href="#collapseHumanResourcesAndManagementOffice"
+        role="button"
+        aria-expanded="false"
+        aria-controls="collapseHumanResourcesAndManagementOffice">
+        <i class="ri-government-line me-2"></i>
+        Human Resources and Management Office
+        <i class="ri-arrow-down-s-line ms-auto"></i>
+      </a>
+
+      <!-- ATTENDANCE SUB MENU -->
+      <div class="collapse ps-3" id="collapseHumanResourcesAndManagementOffice">
+
+        <a href="/pages/maingeneral/menu/human_resources_and_management_office/employee_personal_information.html"
+           id="asidebari_at_hr_employee_personal_information"
+           class="asidebar-link-load-000 list-group-item list-group-item-action fw-semibold border-0 rounded-3 mb-1 fs-5">
+          <i class="ri-user-line me-2"></i> Employee
+        </a>
+
+        <a href="/pages/maingeneral/menu/human_resources_and_management_office/employee_attendance_log.html"
+           id="asidebari_at_hr_employee_attendance_log"
+           class="asidebar-link-load-000 list-group-item list-group-item-action fw-semibold border-0 rounded-3 mb-1 fs-5">
+          <i class="ri-user-line me-2"></i> Attendance Log
+        </a>
+
+        <a href="/pages/maingeneral/menu/human_resources_and_management_office/employee_payroll.html"
+           id="asidebari_at_hr_employee_payroll"
+           class="asidebar-link-load-000 list-group-item list-group-item-action fw-semibold border-0 rounded-3 mb-1 fs-5">
+          <i class="ri-user-line me-2"></i> Payroll
+        </a>
+
+        <a href="/pages/maingeneral/menu/human_resources_and_management_office/employee_dtr.html"
+           id="asidebari_at_hr_employee_dtr"
+           class="asidebar-link-load-000 list-group-item list-group-item-action fw-semibold border-0 rounded-3 mb-1 fs-5">
+          <i class="ri-user-line me-2"></i> Daily Time Record
+        </a>
+
+        <a href="/pages/maingeneral/menu/human_resources_and_management_office/employee_leave_credits.html"
+           id="asidebari_at_hr_employee_leave_credits"
+           class="asidebar-link-load-000 list-group-item list-group-item-action fw-semibold border-0 rounded-3 mb-1 fs-5">
+          <i class="ri-user-line me-2"></i> Employee Leave Credits
+        </a>
+
+
+        <a href="/pages/maingeneral/menu/human_resources_and_management_office/employee_personal_information.html"
+           id="asidebari_at_hr_employee_personal_information"
+           class="asidebar-link-load-000 list-group-item list-group-item-action fw-semibold border-0 rounded-3 mb-1 fs-5">
+          <i class="ri-user-line me-2"></i> Workflow
+        </a>
+        <a href="/pages/maingeneral/menu/human_resources_and_management_office/employee_personal_information.html"
+           id="asidebari_at_hr_employee_personal_information"
+           class="asidebar-link-load-000 list-group-item list-group-item-action fw-semibold border-0 rounded-3 mb-1 fs-5">
+          <i class="ri-user-line me-2"></i> Reports
+        </a>
+        <a href="/pages/maingeneral/menu/human_resources_and_management_office/employee_others.html"
+           id="asidebari_at_hr_employee_others"
+           class="asidebar-link-load-000 list-group-item list-group-item-action fw-semibold border-0 rounded-3 mb-1 fs-5">
+          <i class="ri-user-line me-2"></i> Others
+        </a>
+      </div>
+    `;
+  }
+
+
+
+
+
+
+
+
 
         // =========================
         // MAIN SIDEBAR CONTAINER
@@ -89,18 +113,12 @@ $.ajax({
                     <i class="ri-home-9-line me-2"></i> Home
                 </a>
 
-
                 <!-- REVIEW TIMELINE -->
                 <a href="/pages/maingeneral/menu/review_timeline.html"
                    id="asidebari_review_timeline"
                    class="asidebar-link-load-000 list-group-item list-group-item-action d-flex align-items-center fw-semibold rounded-3 mb-1 fs-5">
                     <i class="ri-timeline-view me-2"></i> Review Timeline
                 </a>
-
-
-
-
-
 
                 <!-- PROFILE -->
                 <a href="/pages/maingeneral/menu/profile_information.html"
@@ -117,9 +135,9 @@ $.ajax({
                     <i class="ri-login-box-line me-2"></i> QR Code Login / Logout                
                 </a>
 
+                ${childPage_HUMAN_RESOURCES_AND_MANAGEMENT_OFFICE}
 
 
-                ${childPage_ATTENDANCE_SYSTEM}
 
             </div>
         `;
@@ -130,34 +148,6 @@ $.ajax({
         console.error('User access error:', xhr.responseText);
     }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
   function mainHome() {
@@ -194,8 +184,47 @@ $.ajax({
           case "/pages/maingeneral/menu/fund_withdrawal.html":
             $("#asidebari_fund_withdrawal").addClass("active");
             break;
-            
 
+
+
+            //HUMAN RESOURCES AND MANAGEMENT OFFICE
+            
+          case "/pages/maingeneral/menu/human_resources_and_management_office/employee_personal_information.html":
+            $("#asidebari_at_human_resources_and_management_office_dropdown").addClass("active");
+            $("#asidebari_at_hr_employee_personal_information").addClass("active");
+            break;
+
+
+          case "/pages/maingeneral/menu/human_resources_and_management_office/employee_attendance_log.html":
+            $("#asidebari_at_human_resources_and_management_office_dropdown").addClass("active");
+            $("#asidebari_at_hr_employee_attendance_log").addClass("active");
+            break;
+          case "/pages/maingeneral/menu/human_resources_and_management_office/employee_attendance_adjustment.html":
+            $("#asidebari_at_human_resources_and_management_office_dropdown").addClass("active");
+            $("#asidebari_at_hr_employee_attendance_adjustment").addClass("active");
+            break;
+            
+          case "/pages/maingeneral/menu/human_resources_and_management_office/employee_payroll.html":
+            $("#asidebari_at_human_resources_and_management_office_dropdown").addClass("active");
+            $("#asidebari_at_hr_employee_payroll").addClass("active");
+            break;
+
+          case "/pages/maingeneral/menu/human_resources_and_management_office/employee_dtr.html":
+            $("#asidebari_at_human_resources_and_management_office_dropdown").addClass("active");
+            $("#asidebari_at_hr_employee_dtr").addClass("active");
+            break;
+
+
+          case "/pages/maingeneral/menu/human_resources_and_management_office/employee_leave_credits.html":
+            $("#asidebari_at_human_resources_and_management_office_dropdown").addClass("active");
+            $("#asidebari_at_hr_employee_leave_credits").addClass("active");
+            break;
+
+          case "/pages/maingeneral/menu/human_resources_and_management_office/employee_others.html":
+            $("#asidebari_at_human_resources_and_management_office_dropdown").addClass("active");
+            $("#asidebari_at_hr_employee_others").addClass("active");
+            break;  
+            
 
           // HUMAN RESOURCES
 
@@ -205,7 +234,7 @@ $.ajax({
             break;
           case "/pages/maingeneral/menu/human_resources/leave_and_requests.html":
             $("#asidebari_at_attendance_system_dropdown").addClass("active");
-            $("#asidebari_at_employee_leave_and_requests").addClass("active");
+            $("#asidebari_at_employee_leave_credits_and_requests").addClass("active");
             break;
           case "/pages/maingeneral/menu/human_resources/payroll_integration.html":
             $("#asidebari_at_attendance_system_dropdown").addClass("active");
